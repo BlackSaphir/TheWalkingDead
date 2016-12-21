af4cf844f21db613840ffddbce959ed6181c0e1e
Romanus <romanusklatt@gmx.de>
2016-12-21 18:21:57 +0100

    Menue feddich

--- Assets/Scripts/Map/DieInWater.cs
+++ Assets/Scripts/Map/DieInWater.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DieInWater : MonoBehaviour
 {
-    void OnCollisionEnter (Collision collision)
+    void OnTriggerEnter(Collider collision)
     {
+        Debug.Log("Pipi");
         if (collision.gameObject.tag == "WaterPlane")
         {
             Destroy(this.gameObject);
+            SceneManager.LoadScene("LooseScene");
         }
     }
 }
---
+++ Assets/Scripts/Menue/ButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void start(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void EndGame(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
