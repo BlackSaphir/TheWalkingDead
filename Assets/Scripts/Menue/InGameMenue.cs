84275e4d6b7c74bec4962a9d0c790d7cfe80cb52
Romanus <romanusklatt@gmx.de>
2016-12-21 18:33:57 +0100

    IngameMenue vegessen

---
+++ Assets/Scripts/Menue/InGameMenue.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenue : MonoBehaviour
+{
+    public GameObject Menuobject;
+    void Start()
+    {
+        Menuobject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                Menuobject.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Menuobject.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+    public void EndGame(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ResumeGame()
+    {
+        Menuobject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
