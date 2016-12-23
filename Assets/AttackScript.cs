c4cdfa8657ed1996de286d06cfba2e19a60e430d
Waldemar Just <Waldemarjust@gmx.de>
2016-12-23 17:34:33 +0100

    [Finished] Hand bugs fixed and Feature Finishes

--- Assets/AttackScript.cs
+++ Assets/AttackScript.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackScript : StateMachineBehaviour {
-
+public class AttackScript : StateMachineBehaviour
+{
+    public float timer;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        timer = 0;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        //animator.SetBool("IsAttacking", false);
+        timer += Time.deltaTime;
+        if (timer > 0.5)
+        {
+            animator.SetBool("IsAttacking", false);
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
